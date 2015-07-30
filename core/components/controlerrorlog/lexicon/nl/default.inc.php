acfcbc0033a43eb9554a40f573a1d0746bef3351
Vierkante Meter <mark@vierkante-meter.nl>
2015-07-30 17:33:58 +0200

    Dutch language file

---
+++ core/components/controlerrorlog/lexicon/nl/default.inc.php
@@ -0,0 +1,11 @@
+<?php
+$_lang['no'] = 'Nee';
+$_lang['yes'] = '<span>Ja</span>';
+$_lang['errors'] = 'Fouten';
+$_lang['refresh'] = 'Verversen';
+$_lang['clear'] = 'Legen';
+$_lang['error_log'] = 'Foutlog';
+$_lang['errors_title'] = 'Open de foutlog in een nieuw venster';
+$_lang['error_log_last_lines'] = 'De laatste [[+last]] regels.';
+$_lang['error_log_download'] = 'Download foutlog ([[+size]])';
+$_lang['error_log_too_large'] = 'De foutlog op <em>[[+name]]</em> is te groot om weer te geven. Download de foutlog via onderstaande knop.';
