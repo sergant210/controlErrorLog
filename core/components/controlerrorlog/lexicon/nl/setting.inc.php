db7a7d43a88e5c64b871cdf9b72379dd518ef93c
Vierkante Meter <mark@vierkante-meter.nl>
2016-11-24 10:54:26 +0100

    Update setting.inc.php

--- core/components/controlerrorlog/lexicon/nl/setting.inc.php
+++ core/components/controlerrorlog/lexicon/nl/setting.inc.php
@@ -9,5 +9,5 @@ $_lang['setting_controlerrorlog.auto_refresh'] = 'Automatisch vernieuwen';
 $_lang['setting_controlerrorlog.auto_refresh_desc'] = 'Of het foutlog automatisch moet worden ververst na het gespecificeerd aantal seconden.';
 $_lang['setting_controlerrorlog.control_frontend'] = 'Informeren over wijzigingen';
 $_lang['setting_controlerrorlog.control_frontend_desc'] = 'Informeren over wijzigingen.';
-$_lang['setting_controlerrorlog.admin_email'] = 'Admin e-mail';
-$_lang['setting_controlerrorlog.admin_email_desc'] = 'Admin e-mail te informeren over wijzigingen in de foutlog.';
+$_lang['setting_controlerrorlog.admin_email'] = 'E-mailadres administrator';
+$_lang['setting_controlerrorlog.admin_email_desc'] = 'Indien er iets in het foutlog wordt geschreven, zal er naar dit e-mailadres een notificatie worden verzonden.';
